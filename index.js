49df8c2c0de8efa917550334135290c72c096f4d
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-17 13:28:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,29 +18,7 @@ function takeANumber (nPeople, name)
 takeANumber(katzDeli, 'Steven')
 
 
-function nowServing ()
 
-
-{
-  var first = [...katzDeli]
-  console.log(first[0])
-  console.log(katzDeli.length)
-  
-  
-  if (katzDeli.length === 1)
-  
-  {
-    
-    return `Currently serving ` + first[0];
-
-  }
-  
-   return "There is nobody waiting to be served!";
-    
-  
-  }
-
-nowServing();
 
 
 
