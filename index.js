94ef65e99dd5385253b2e564b85fe8c7bee0f9ee
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-18 13:37:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var katzDeli = [];
-
+var i = 0;
 function takeANumber (katzDeliLine, newName)
 
 {
   
-  
+  katzDeliLine[i] = newName;
+  i++
+  return `Welcome ${newName} you are number ${i-1} in line.`
   
 }
+
+console.log(takeANumber(katzDeli,'Ada'))
