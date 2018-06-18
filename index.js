ca7c94cc65ad0d75815666a4679f57908b202681
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-18 14:36:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 var katzDeli = [];
-function takeANumber (katzDeliLine, newName)
 
-{
-  
-  katzDeliLine.push(newName);
-  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
-  
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-takeANumber(katzDeli, 'Steven')
-function nowServing ()
-
-{
-  
-  var deliLine = katzDeli[0];
-  
-  console.log(katzDeli.length)
-  if (!katzDeli.length )
-  
-  
-  {
-    
-    return 'There is nobody waiting to be served!';
-    
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
   }
+  var lineNamesandNumbers = [];
   
-
-  katzDeli.shift();
-  return `Currently serving ${deliLine}.`;
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
 
-nowServing();
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    //console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
 
+function takeANumber(line, name){
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+takeANumber(katzDeli, "Ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+currentLine(katzDeli);
+nowServing(katzDeli);
+takeANumber(katzDeli, "Matz"); 
+currentLine(katzDeli); 
+nowServing(katzDeli);
+currentLine(katzDeli)
