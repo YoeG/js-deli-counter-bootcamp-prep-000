c7b74eb0c1416c47bc5e5d09c20bbcac109bc1a3
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-18 02:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-
-var i;
 var katzDeli = [];
-console.log(katzDeli.length)
-function takeANumber (katzDeli, name)
+
+function takeANumber (katzDeliLine, newName)
 
 {
-  katzDeli[i] = name
-  i++;
-  return `Welcome, ${name}. You are number ${parseInt(katzDeli.length)} in line.`
-}
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-takeANumber(katzDeli, 'Bill')
-takeANumber(katzDeli, 'Jane')
-takeANumber(katzDeli, 'Ann')
-console.log(katzDeli)
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-function currentLine ()
-
-
-{
-
-
+  
+  
+  
 }
