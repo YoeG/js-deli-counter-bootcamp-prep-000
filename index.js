f61cad68e62a58e79eb9697ad6fcf775ecd25221
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-18 13:37:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber (katzDeliLine, newName)
   
   katzDeliLine[i] = newName;
   i++
-  return `Welcome ${newName} you are number ${i-1} in line.`
+  return `Welcome ${newName} you are number ${i} in line.`
   
 }
 
